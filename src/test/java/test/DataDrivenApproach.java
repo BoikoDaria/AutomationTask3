8bd709ebdc90dc507a417b543867f6f70ee1b4d7
BoikoDaria <boiko.daria@gmail.com>
2018-10-16 14:38:22 +0300

    complete tests with data driven approach

--- src/test/java/test/DataDrivenApproach.java
+++ src/test/java/test/DataDrivenApproach.java
@@ -1,40 +1,33 @@
 package test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import objects.CalculatorObjects;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.Collection;
 
+@RunWith(Parameterized.class)
 public class DataDrivenApproach extends FirstTests {
 
     @Parameterized.Parameter
     public String expression;
 
-    @Parameterized.Parameter
+    @Parameterized.Parameter(1)
     public String expected;
 
 
-    CalculatorObjects calculator;
+    private static CalculatorObjects calculator;
 
-    @Before
-    public void openCalc() {
+    @BeforeClass
+    public static void openCalc() {
         calculator = new CalculatorObjects(driver);
     }
 
-    @BeforeClass
-    public static void startup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        String basePath = new File("").getAbsolutePath();
-        driver.get("file://" + basePath + "/calc.html");
+    @After
+    public void clearCalc() {
+        calculator.click_clean();
     }
 
     @Test
@@ -50,8 +43,8 @@ public class DataDrivenApproach extends FirstTests {
                 {"100/2", "50"},
                 {"230-30", "200"},
                 {"0-14", "-14"},
-                {"5.03+2.001", "5.031"},
-                {"78413*69856", "5477618528"},
+                {"5.03+2.001", "7.031000000000001"},
+                {"78413x69856", "5477618528"},
                 {"256901/236", "1088.5635593220338"},
                 {"12413/-6956", "-1.784502587694077"},
                 {"6.13+0.13", "6.26"},
